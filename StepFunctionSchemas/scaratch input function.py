d251ee7782d0ab11be21de3036e14653a1cf3701
paul-in-the-cloud <pguidry@2ndwatch.com>
2021-04-08 04:22:55 -0400

    added state fucntions

---
+++ StepFunctionSchemas/scaratch input function.py
@@ -0,0 +1,22 @@
+'''
+
+$.foo => 123
+$.bar => ["a", "b", "c"]
+$.car.cdr => true
+
+
+$.table_name": "tab_1",
+$.table_schema_name": "dbo",
+$.db_name": "database_name",
+$.db_port": "1433",
+$.db_instance": "hkkkkh",
+$.db_user": "pgadmin",
+$.db_password": "1#peanuts",
+$.db_max_retries": 2,
+$.row_count": 12000,
+$.column_list": ["col1","col2","col3","col4","col5"],
+$.column_pk_list": ["col1"],
+$.data_type_list": ["int","varchar","varchar","varchar","varchar"],
+$.con_time_out_sec": 60,
+$.log_level": "debug"}
+'''
